16d02254783bd597fd1af44f41dc6f2efa4f01a7
Condensator <conder.ua@gmail.com>
2022-01-30 15:54:21 +0200

    Delete dbo directory

--- dbo/Tables/Creation-Scripts/table1646.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE [dbo].[table1646](	  [PersonID] INT  NULL	, [LastName] VARCHAR(255) COLLATE Latin1_General_CI_AS  NULL	, [FirstName] VARCHAR(255) COLLATE Latin1_General_CI_AS  NULL	, [Address] VARCHAR(255) COLLATE Latin1_General_CI_AS  NULL	, [City] VARCHAR(255) COLLATE Latin1_General_CI_AS  NULL)GO
--- dbo/Tables/Creation-Scripts/table730.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE [dbo].[table730](	  [PersonID] INT  NULL	, [LastName] VARCHAR(255) COLLATE Latin1_General_CI_AS  NULL	, [FirstName] VARCHAR(255) COLLATE Latin1_General_CI_AS  NULL	, [Address] VARCHAR(255) COLLATE Latin1_General_CI_AS  NULL	, [City] VARCHAR(255) COLLATE Latin1_General_CI_AS  NULL)GO
